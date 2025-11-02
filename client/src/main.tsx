3ea679d2fd37732617658eeed63431a26a015649
Ecobhangar <abhysmo@gmail.com>
2025-11-02 19:04:02 +0530

    fix: wrap app in QueryClientProvider

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,11 +3,18 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { AuthProvider } from "./context/AuthContext";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+// Create QueryClient instance
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    {/* Provide React Query client and Auth context globally */}
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
