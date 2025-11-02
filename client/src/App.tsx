35feb23156dce7fa0967ee43d5d970bd87216996
Ecobhangar <abhysmo@gmail.com>
2025-11-02 13:59:17 +0530

    fix: corrected _redirects spacing for SPA routing on Render

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,24 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
+import CreateBooking from "./pages/create-booking";
+import NotFound from "./pages/not-found";
 
 export default function App() {
   return (
     <Router>
       <Routes>
+        {/* ✅ Default route */}
         <Route path="/" element={<Home />} />
+
+        {/* ✅ Login page */}
         <Route path="/login" element={<Login />} />
-        <Route path="/book" element={<Login />} /> {/* temporary reuse */}
+
+        {/* ✅ Book pickup / create booking */}
+        <Route path="/book" element={<CreateBooking />} />
+
+        {/* ✅ Catch-all fallback route */}
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
   );
